a56c725f0dd992dae2478afae3346392e0823d5a
ndjeundo <thierryndjeundo@gmail.com>
2020-02-05 13:30:40 +0100

    RestRepository

--- salvo/src/main/java/com/codeoftheweb/salvo/PlayerRepository.java
+++ salvo/src/main/java/com/codeoftheweb/salvo/PlayerRepository.java
@@ -2,9 +2,8 @@ package com.codeoftheweb.salvo;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import java.util.List;
-
+@RepositoryRestResource
 public interface PlayerRepository extends JpaRepository<Player, Long> {
-    List<Player> findByLastName(String lastName);
 }
